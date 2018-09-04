0fe38b1f43b653583b03e702908ba1b40df8a085
Lakshmi K <klakshmi75@gmail.com>
2018-09-04 15:35:53 +0530

    Completed part 4 of spring guru tutorial - thyme leaf templates.

---
+++ src/main/java/com/example/demo/controllers/ProductController.java
@@ -0,0 +1,56 @@
+package com.example.demo.controllers;
+
+import com.example.demo.model.Product;
+import com.example.demo.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ProductController {
+    private ProductService productService;
+
+    @Autowired
+    public void setProductService(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @RequestMapping(value="product/new")
+    public String newProduct(Model model) {
+        model.addAttribute("product", new Product());
+        return "productform";
+    }
+
+    @RequestMapping(value="product", method=RequestMethod.POST)
+    public String saveProduct(Product product) {
+        productService.saveProduct(product);
+        return "redirect:/product/" + product.getId();
+    }
+
+    @RequestMapping(value="product/{id}")
+    public String showProduct(@PathVariable Integer id, Model model) {
+        model.addAttribute("product", productService.getProductById(id));
+        return "productshow";
+    }
+
+    @RequestMapping(value="products", method=RequestMethod.GET)
+    public String list(Model model) {
+        model.addAttribute("products", productService.listAllProducts());
+        return "products";
+    }
+
+    @RequestMapping(value="product/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model) {
+        model.addAttribute("product", productService.getProductById(id));
+        return "productform";
+    }
+
+    @RequestMapping(value="product/delete/{id}")
+    public String delete(@PathVariable Integer id) {
+        productService.deleteProduct(id);
+        return "redirect:/products";
+    }
+}
---
+++ src/main/java/com/example/demo/services/impl/ProductRepositoryService.java
@@ -0,0 +1,40 @@
+package com.example.demo.services.impl;
+
+import com.example.demo.model.Product;
+import com.example.demo.repositories.ProductRepository;
+import com.example.demo.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.xml.ws.ServiceMode;
+
+@Service
+public class ProductRepositoryService implements ProductService{
+    private ProductRepository productRepository;
+
+    @Autowired
+    public void setProductRepository(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public Iterable<Product> listAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public Product getProductById(Integer id) {
+        return productRepository.findById(id).get();
+    }
+
+    @Override
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Override
+    public void deleteProduct(Integer id) {
+        productRepository.deleteById(id);
+    }
+
+}
